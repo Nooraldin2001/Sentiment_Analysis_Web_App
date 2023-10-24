ad7b99ba2a5be477ce8ed404c670dfdba82658b2
Nooraldin2001 <noorabdalkadir@std.mans.edu.eg>
2023-10-24 16:36:12 +0300

    Task 6: Deploy as web application using Flask

--- server.py
+++ server.py
@@ -5,9 +5,11 @@
 # Import Flask, render_template, request from the flask pramework package : TODO
 from flask import Flask, render_template, request
 # Import the sentiment_analyzer function from the package created: TODO
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
 #Initiate the flask app : TODO
-app = Flask(__name__)
+app = Flask("Sentiment Analyzer")
+
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
     ''' This code receives the text from the HTML interface and 
@@ -16,6 +18,12 @@ def sent_analyzer():
         score for the provided text.
     '''
     # TODO
+    text_to_analyze = request.args.get('textToAnalyze')
+    reponse = sentiment_analyzer(text_to_analyze)
+    label = reponse['label'] 
+    score = reponse['score'] 
+    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+
 
 @app.route("/")
 def render_index_page():
@@ -23,7 +31,9 @@ def render_index_page():
         page over the Flask channel
     '''
     #TODO
+    return render_template('index.html')
 
 if __name__ == "__main__":
     ''' This functions executes the flask app and deploys it on localhost:5000
     '''#TODO
+    app.run(host="0.0.0.0", port=5000)
