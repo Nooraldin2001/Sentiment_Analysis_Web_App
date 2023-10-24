307fd24a5c261b0af9fb9ee3dddeeaca472a223a
Nooraldin2001 <noorabdalkadir@std.mans.edu.eg>
2023-10-24 23:05:00 +0300

    Task 8: Run static code analysis

--- server.py
+++ server.py
@@ -4,10 +4,9 @@
 '''
 # Import Flask, render_template, request from the flask pramework package : TODO
 from flask import Flask, render_template, request
-# Import the sentiment_analyzer function from the package created: TODO
+# Import the sentiment_analyzer function from the package created:
 from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
-
-#Initiate the flask app : TODO
+#Initiate the flask app :
 app = Flask("Sentiment Analyzer")
 
 @app.route("/sentimentAnalyzer")
@@ -17,26 +16,20 @@ def sent_analyzer():
         function. The output returned shows the label and its confidence 
         score for the provided text.
     '''
-    # TODO
     text_to_analyze = request.args.get('textToAnalyze')
     reponse = sentiment_analyzer(text_to_analyze)
-    label = reponse['label'] 
-    score = reponse['score'] 
+    label = reponse['label']
+    score = reponse['score']
     if label is None:
         return "Invalid input ! Try again."
-    else:
-        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
-
+    return f"The given text has been identified as {label.split('_')[1]} with a score of {score}."
 
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
-    #TODO
     return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
     app.run(host="0.0.0.0", port=5000)
